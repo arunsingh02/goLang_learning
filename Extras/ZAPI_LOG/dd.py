3f8c57fccefb561bcbe2293a8e2433f9c7477275
Arun Singh <aruns7@Aruns-MacBook-Pro.local>
2023-04-24 17:10:42 +0530

    removed unwanted files and codes

--- Extras/ZAPI_LOG/dd.py
+++
@@ -1,6 +0,0 @@
-a = "{\"statusResp\":{\"product\": \"FDvM300\", \"serialNumber\": \"320000030\", \"version\": \"1\", \"licenses\": {\"startDate\": \"2023-01-16\", \"HostID\": \"a5db8478-2ad1-11ed-a4b4-000c29c2b6e0\", \"capacity\": \"10\", \"package\": \"CapacityPool\", \"licenseProtocol\": \"OTS-M300_PRM_CP_SUB\", \"endDate\": \"2023-06-16\", \"type\": \"capacity-per-term\"}, \"message\": \"Success\", \"snStatus\": \"Active\", \"statusCode\": \"S007\"},\"Signature\":\"fhZnewnO6ubFXxL7IOU00X8ojzKq6wc81BPreqj3qBoj/6rURaelV446w4aUbWYEPpfetr6RXWPy84HkGjz9+HBYwnV2KzSoELd0umpP1GFVDssyEXemTLjfQWfcbwg7BsOQGL/behQBKpCFXKSa5D+PIjj+bjzdT/QufOJs2LCr4GWbk3EdZ23c+jzYcQfm1gvtfNJlQEz6X+S1vuGUWcBTmJB5eocUKTBvvIsNWU7KI0vkoYsSSE89tPGGhY+f4XnuxXefkAyzQ8aosfIkpqu2VSOmSwU4mKBSAAAUl1O1Xwr614R8KKFniMrY1sXGrWcRA1fO6ifdCDa0Ndpaww==\"}"
-
-from pprint import pprint
-
-
-pprint(a)
